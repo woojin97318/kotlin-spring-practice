0bf479ae47f36b401ce04f0e42cba99ed4c47859
전우진 <woojin97318@gmail.com>
2024-07-16 16:50:42 +0900

    inital commit

---
+++ src/test/kotlin/com/example/kotlinspringpractice/KotlinSpringPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringpractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringPracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
